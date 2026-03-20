be15ccc8408ee394286122a7785a44d13a9a7adf
Chengyou Xiang <eric759920033xcyss@gmail.com>
2026-03-20 12:39:22 +0000

    add Aim1

--- pipeline_scripts/imputation.sh
+++ pipeline_scripts/imputation.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 #PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=32:mem=200gb
-#PBS -N pred_no_skip
-
-#PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=128:ompthreads=128:mem=200gb
 #PBS -N imputation
-
-cd /rds/general/project/hda_25-26/live/TDS/TDS_Group8/pipeline_scripts/
 
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
 conda activate r413
 
-Rscript imputation_no_skip.R
+Rscript pipeline_scripts/imputation.R
