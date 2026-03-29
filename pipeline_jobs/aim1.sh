e30fa8d1e3522f79f2beeb329bc952fa009bfb59
Chengyou Xiang <eric759920033xcyss@gmail.com>
2026-03-29 00:21:40 +0000

    debug

--- pipeline_jobs/aim1.sh
+++ pipeline_jobs/aim1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=00:30:00
 #PBS -l select=1:ncpus=1:mem=60gb
-#PBS -N preprocessing
+#PBS -N aim1
 
 cd "$PBS_O_WORKDIR/pipeline_scripts"
 #cd /rds/general/project/hda_25-26/live/TDS/TDS_Group8/pipeline_scripts
