a29889af75691e9acb7c65a465edd846976efc64
Chengyou Xiang <eric759920033xcyss@gmail.com>
2026-03-30 01:56:44 +0100

    update extraction scripts

--- extraction_and_recoding/3-recode_extracted.sh
+++ extraction_and_recoding/3-recode_extracted.sh
@@ -2,11 +2,10 @@
 #PBS -l select=1:ncpus=1:mem=80gb
 #PBS -N recoding
 
-cd "$PBS_O_WORKDIR/extraction_and_recoding/scripts"
-#cd /rds/general/project/hda_25-26/live/TDS/TDS_Group8/extraction_and_recoding/scripts
+cd "$PBS_O_WORKDIR/extraction_and_recoding/scripts" || cd /rds/general/project/hda_25-26/live/TDS/TDS_Group8/extraction_and_recoding/scripts || exit 1
 
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
-conda activate r413
+conda activate g8-r413
 
 Rscript 3-recode_variables.R
 
