7c9b30f38a86680cf1f8148931fbcce150270a3a
Chengyou Xiang <eric759920033xcyss@gmail.com>
2026-03-30 01:55:11 +0100

    update extraction scripts

--- extraction_and_recoding/2-extract_selected.sh
+++ extraction_and_recoding/2-extract_selected.sh
@@ -2,13 +2,12 @@
 #PBS -l select=1:ncpus=1:mem=80gb
 #PBS -N extraction
 
-cd "$PBS_O_WORKDIR/extraction_and_recoding/scripts"
-#cd /rds/general/project/hda_25-26/live/TDS/TDS_Group8/extraction_and_recoding/scripts
+cd "$PBS_O_WORKDIR/extraction_and_recoding/scripts" || cd /rds/general/project/hda_25-26/live/TDS/TDS_Group8/extraction_and_recoding/scripts || exit 1
 
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
-conda activate r413
+conda activate g8-r413
 
-ukb_path=/rds/general/project/hda_25-26/live/TDS/General/Data/tabular.tsv
+ukb_path=/rds/general/project/hda_25-26/live/TDS/General/Data/tabular.tsv #update with actual ukbiobank path
 
 Rscript 2-extract_selected.R $ukb_path 
 
