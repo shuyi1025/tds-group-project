244319c1f41fec201a435697806ef479d5877ee9
ayesha-hus <ayeshahussain288@gmail.com>
2026-03-30 17:49:30 +0100

    Update nn_bio.sh

--- pipeline_jobs/nn_bio.sh
+++ pipeline_jobs/nn_bio.sh
@@ -2,8 +2,8 @@
 #PBS -l select=1:ncpus=10:mem=64gb
 #PBS -l walltime=24:00:00
 #PBS -j oe
-#PBS -o nn_bio.log
-#PBS -e nn_bio.log
+#PBS -o nn_bio.out
+#PBS -e nn_bio.err
 
 cd "$PBS_O_WORKDIR/pipeline_scripts/Aim4" || cd /rds/general/project/hda_25-26/live/TDS/TDS_Group8/pipeline_scripts/Aim4 || exit 1
 
